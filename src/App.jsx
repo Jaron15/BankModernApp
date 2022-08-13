81b8531fc2ddb5728b9665528ab54198de00fa32
Jaron <jjkenyon777@gmail.com>
2022-08-12 20:56:53 -0700

    component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import styles from './style'
+import React from 'react';
+import styles from './style';
+import { Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero, } from './components'
+
+
+
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
 
         <div className={`bg-primary ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-            Hero
+            <Hero />
           </div>
         </div>
         <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-            Stats
-            Business
-            CardDeal
-            testimonials 
-            clients
-            cta
-            footer
+            <Stats />
+            <Business />
+            <CardDeal />
+            <Testimonials /> 
+            <Clients />
+            <CTA />
+            <Footer />
           </div>
         </div>
 
